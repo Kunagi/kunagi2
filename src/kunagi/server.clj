1a7fcc645304c24da7e7810eb8238906677a634e
Witoslaw Koczewski <wi@koczewski.de>
2019-02-01 00:05:36 +0100

    apptoolkit upgrade

--- src/kunagi/server.clj
+++ src/kunagi/server.clj
@@ -1,13 +1,24 @@
 (ns kunagi.server
   (:require
+   [compojure.route :as compojure-route]
+
+   [appkernel.integration :as integration]
+   [appkernel.api :as app]
    [apptoolkit.mod.http-server :as http-server]))
 
 
+;; TODO use appkernel lifecycle
+(defn start! [options]
+  (tap> [::start! options])
+  (-> (integration/db)
+      (merge options)
+      (http-server/start!)))
 
-(defn start! []
-  (prn "START!")
-  ;; (app/start!))
-  (http-server/start! {}))
+
+(defn -main [port]
+  (tap> ::main)
+  (start! {:http-server/port (Integer/parseInt port)
+           :http-server/app-js-path "cljs-out/prod-main.js"}))
 
 
 (defn -main [& args]
@@ -18,9 +29,9 @@
   (if (and (= :get (:request-method request))
            (= "/"  (:uri request)))
     (do
-      (start!)
+      (start! {})
       {:status 302
-       :headers {"Location" (str "http://localhost:" http-server/port)}})
+       :headers {"Location" (str "http://localhost:" http-server/dev-port)}})
     {:status 404
      :headers {"Content-Type" "text/plain"}
      :body "404 - Page not found"}))
