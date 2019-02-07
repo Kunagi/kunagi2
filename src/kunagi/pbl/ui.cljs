9ff53c794cce473658277fda583016ac966d22e4
Witoslaw Koczewski <wi@koczewski.de>
2019-02-07 20:35:30 +0100

    pbl: expandable-panel-list

--- src/kunagi/pbl/ui.cljs
+++ src/kunagi/pbl/ui.cljs
@@ -6,43 +6,27 @@
    [appkernel.api :as app]
    [facts-db.api :as db]
    [material-desktop.desktop :as desktop]
-   [material-desktop.components :as mdc]))
+   [material-desktop.components :as mdc]
+   [material-desktop.expansion-panel-list :as expansion-panel-list]))
 
-
-(defn ProductBacklogItemHead [item]
-  [mdc/CardContent
-   [:> icons/ExpandMore
-    {:style {:float :right}}]
-   [mdc/Text
-    (:label item)]])
-
-
-(defn ProductBacklogItemContent [item]
-  [:div "hidden item content"])
-
-
-(defn ProductBacklogItem [item]
-  [:div.ProductBacklogItem
-   [mdc/Card
-    [ProductBacklogItemHead item]]])
 
 
 (defn ProductBacklog []
-  [:div#ProductBacklog
-   [:div
-    {:style {:margin-bottom "1rem"}}
-    [mdc/Button {:variant :contained
-                 :on-click #(rf/dispatch [:kunagi/pbl-item-created {:pbl "some-random-pbl-id"
-                                                                    :item {:db/id (str (random-uuid))
-                                                                           :label "New item"}}])}
-                ;;[:> icons/Add]
-                "Add Product Backlog Item"]]
+  (let [pbl @(rf/subscribe [::pbl])]
+    [:div#ProductBacklog
+     [:pre (str "pbl: " pbl)]
+     ;; [:pre (str "sub: " @(rf/subscribe [:app/projection-db {:name :kunagi/pbl :args {}}]))]
+     [:div
+      {:style {:margin-bottom "1rem"}}
+      [mdc/Button {:variant :contained
+                   :on-click (:add-item-on-click pbl)}
+                  ;;[:> icons/Add]
+       "Add Product Backlog Item"]]
+     [expansion-panel-list/ExpansionPanelList
+      {:panels (map (fn [pbl-item]
+                      {:summary {:text (:label pbl-item)}})
+                    (:items pbl))}]]))
 
-   [:pre (str "pbl: " @(rf/subscribe [::product-backlog]))]
-   [:pre (str "sub: " @(rf/subscribe [:app/projection-db {:name :kunagi/pbl :args {}}]))]
-   [mdc/CardsColumn
-    {:cards (into (map (fn [item] [ProductBacklogItem item])
-                       (:items @(rf/subscribe [::product-backlog]))))}]])
 
 (defn Workarea []
   [ProductBacklog])
@@ -63,8 +47,14 @@
 ;;; subscriptions
 
 (rf/reg-sub
- ::product-backlog
- (fn [_ _]
+ ::pbl
+ (fn [_]
    (rf/subscribe [:app/projection-db {:name :kunagi/pbl}]))
  (fn [projection-db _]
-   (db/tree projection-db "some-random-pbl-id" {:items {}})))
+   (let [pbl-id "some-random-pbl-id"]
+     (-> (db/tree projection-db pbl-id {:items {}})
+         (assoc :add-item-on-click
+                #(rf/dispatch [:kunagi/pbl-item-created
+                               {:pbl pbl-id
+                                :item {:db/id (str (random-uuid))
+                                       :label (str "New item " (.getTime (js/Date.)))}}]))))))
