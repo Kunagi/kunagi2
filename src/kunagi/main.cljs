1f914461f339b47440054486630749c61ca05b2c
Witoslaw Koczewski <wi@koczewski.de>
2019-06-13 10:27:24 +0200

    refactoring

--- src/kunagi/main.cljs
+++ src/kunagi/main.cljs
@@ -7,6 +7,7 @@
 
    [apptoolkit.domain-model-editor.mod]
 
+   [material-desktop.desktop.domain-model-module :as desktop-domain]
    [kunagi.scrum.domain-model-module :as scrum-domain]
    [kunagi.browserapp.domain-model-module :as browserapp-domain]
    [kunagi.events]
@@ -23,6 +24,7 @@
   (browserapp/start
    config-edn
    [Root]
-   {:domain-model/modules-events {:scrum      scrum-domain/events
-                                  :browserapp browserapp-domain/events}})
+   {:domain-model/modules-events {:scrum scrum-domain/events
+                                  :desktop desktop-domain/events}})
+                                  ;;:browserapp browserapp-domain/events}})
   (rf/dispatch-sync [:kunagi/init]))
