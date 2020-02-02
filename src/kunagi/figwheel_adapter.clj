05676002fb93f71f9a11e352b54a7dd8af80680c
Witoslaw Koczewski <wi@koczewski.de>
2020-02-02 16:56:46 +0100

    cleanup (remove figwheel)

--- src/kunagi/figwheel_adapter.clj
+++
@@ -1,7 +0,0 @@
-(ns kunagi.figwheel-adapter
-  (:require
-   [apptoolkit.http-server.figwheel-adapter :as figwheel-adapter]
-
-   [kunagi.main :as main]))
-
-(def ring-handler-for-figwheel (figwheel-adapter/ring-handler-for-figwheel main/-main))
