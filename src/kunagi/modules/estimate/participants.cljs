8b4f765d2dc9d70d7952b644654f915c25e034ce
Witoslaw Koczewski <wi@koczewski.de>
2020-02-02 16:32:35 +0100

    participants state

---
+++ src/kunagi/modules/estimate/participants.cljs
@@ -0,0 +1,9 @@
+(ns kunagi.modules.estimate.participants)
+
+
+(defn new-state []
+  {:participants {"p1" {:id "p1"
+                        :name "Alice"}
+                  "p2" {:id "p2"
+                        :name "Bob"}}})
+
