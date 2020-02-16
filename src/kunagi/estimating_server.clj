3df5f5a5f43ffcdd783f261ed90a5f785d43129e
Artjom Weyand <artjom.kochtchi@googlemail.com>
2020-02-16 21:54:43 +0100

    fixed broadcast

--- src/kunagi/estimating_server.clj
+++ src/kunagi/estimating_server.clj
@@ -4,24 +4,21 @@
    [kunagi.estimating :as estimating]))
 
 
-(defonce !state (atom {:estimating-state (estimating/new-state)}))
-
+(defonce !state (atom {:estimating-states {}
+                       :estimating-state (estimating/new-state)}))
 
 
 (defn on-client-event [[event-ident event] context]
   (tap> [:!!! ::on-client-event event])
-  (let [response-f (-> context :comm/response-f)
-        send-fn (-> @http-server/!sente-socket :send-fn)
+  (let [send-fn (-> @http-server/!sente-socket :send-fn)
         connected-uids (-> @http-server/!sente-socket :connected-uids deref :any)
         broadcast-f (fn [event]
                       (doseq [uid connected-uids]
-                        (tap> [:!!! ::respond-to uid])
-                        (send-fn uid event)))
+                        (send-fn uid [:kunagi-base/event event])))
         estimating-state (-> @!state :estimating-state)
         estimating-state (case (-> event :event-name)
                            :kunagi/device-connected estimating-state
                            (estimating/apply-event estimating-state event))]
     (swap! !state assoc :estimating-state estimating-state)
-    (broadcast-f [:kunagi/estimating-state-received estimating-state]) ;; FIXME
-    (response-f [:kunagi/estimating-state-received estimating-state]))
+    (broadcast-f [:kunagi/estimating-state-received estimating-state]))
   context)
